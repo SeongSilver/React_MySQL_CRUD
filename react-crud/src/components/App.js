cdeb17c7a13d1eca6d4af36ecbdc871ce9c5714c
BABY\SEONGEUN <yooseongeun@naver.com>
2022-09-21 18:23:23 +0900

    Redux 사용 전까지 component 세팅

---
+++ react-crud/src/components/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import BoardPage from './views/BoardPage/BoardPage'
+import ArticlePage from './views/ArticlePage/ArticlePage';
+import RegisterPage from './views/RegisterPage/RegisterPage';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route exact path="/" element={<BoardPage/>}/>
+        <Route exact path="/article/:articleId" element={<ArticlePage/>}/>
+        <Route exact path="/register" element={<RegisterPage/>}/>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-crud/src/components/views/ArticlePage/ArticlePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ArticlePage = () => {
+    return(
+        <div>
+            articlePage
+        </div>
+    )
+}
+
+export default ArticlePage;
