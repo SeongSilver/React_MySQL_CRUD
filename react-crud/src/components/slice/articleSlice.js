03be65910248fd5192097337429c83ec4244a1cd
SeongSilver <95121282+SeongSilver@users.noreply.github.com>
2022-09-23 18:41:11 +0900

    Update articleSlice.js

--- react-crud/src/components/slice/articleSlice.js
+++ react-crud/src/components/slice/articleSlice.js
@@ -1,5 +1,8 @@
 import {createSlice} from '@reduxjs/toolkit';
 
+//리덕스 모듈 하나를 만들려면 action type을 정의하고, action creator를 만들고
+//redux-saga를 사용하는 경우 saga를 만들고, reducer까지 만들어야 하는데 이 모든게 (saga 빼고)
+//createSlice()한번에 가능하다
 export const articleSlice = createSlice({
     name:'article',
     initialState: {id: 0, title:"", content:"", views:0},
